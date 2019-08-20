bb021ee8581b1e045ceb1d215c52eb4662cf2fc4
davidkanaa <david.kanaa@gmail.com>
2019-08-20 14:35:49 -0400

    Changed name 'sampler' to 'looper' in order to avoid confusion with an actual sampler.

--- examples/latent_ode_moving_mnist.py
+++ examples/latent_ode_moving_mnist.py
@@ -306,7 +306,7 @@ def make_grid_(tensor, nrow=8, padding=2,
     return array
 
 
-def sampler(x):
+def looper(x):
     while True:
         for e in x:
             yield e
@@ -389,7 +389,7 @@ if __name__ == '__main__':
     dl = moving_mnist_ode_data_loader(args.data_path, num_objects=[args.num_digits], batch_size=args.num_of_samples,
                                         n_frames_input=args.n_frames_input, n_frames_output=args.n_frames_output,
                                         n_workers=8)
-    data_loader = sampler(dl)
+    data_loader = looper(dl)
     data_writer = Writer(select_dim=1, group_dim=-1, group_padding=2, group_pad_value=1, grid_padding=8, grid_pad_value=1, fps=2)
 
     orig_trajs, samp_trajs, orig_ts, samp_ts = next(data_loader)
